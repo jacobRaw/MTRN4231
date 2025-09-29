e3af2260926bf74df16d3af3c2480a9a5befae52
jacob <jacobrawung1@hotmail.com>
2025-09-29 21:04:47 +1000

    Revert "started lab3"
    This reverts commit 3b214c5b65df2362ba594955b1a7b2b16157754e.

--- lab3_workspace/src/object_detect/build/object_detect/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jacob/MTRN4231/lab3_workspace/src/object_detect/install/object_detect'
